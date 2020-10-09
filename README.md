0ecc2d2497ab3b53bf0732079ca7bc28c30a7a45
Reinoptland <reinoptland@gmail.com>
2020-10-09 14:11:59 +0200

    Configure database, put credentials in .env

---
+++ README.md
@@ -0,0 +1,9 @@
+## README
+
+Dear colleague
+
+In order for this to run you need to have a .env file in the following format
+
+```
+DATABASE_URL_DEVELOPMENT=<your url here>
+```
---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+console.log("WHAT IS ENV", process.env);
+
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL_DEVELOPMENT,
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
